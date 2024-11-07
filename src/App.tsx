00bc538754738e34b71f269ee2e9d9af5f7ba6fd
raksiarz <raksiarz@gmail.com>
2024-11-07 13:14:29 +0100

    batman

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <mesh />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
